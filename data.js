0138c0c63d8371cde0dee36b8b78a5c3d1f4b0f1
AL <146753285+arnl2025@users.noreply.github.com>
2026-01-22 11:43:14 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const SITES = [
   { title: "Blogg", url: "https://exempel.se", description: "Blogg om kod", keywords: "blogg kod javascript" },
-  { title: "Docs", url: "https://docs.exempel.se", description: "API-dokumentation", keywords: "api guide documentation" },
+  { title: "Prowear", url: "https://arnl2025.github.io/prowear/index.html", description: "Klädesbutik", keywords: "tröja byxor pro wear butik kläder" },
   { title: "Nordic Golf Shop", url: "https://nordicgolfshop.github.io/nordicgolfshop/", description: "Golfbutik", keywords: "pegg bollar nordic golf" }
 ];
